41c85aac488dbc5fc8e1dce0e8f8c65a3e0fb531
unknown <kielbermudez200104@gmail.com>
2025-06-12 23:46:13 +0800

    button testing M

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="bg-red-100">
       <h1>dasdasdasd</h1>
-      <Button>Hhaha commit test</Button>
+      <Button>dasd</Button>
     </div>
   )
 }
